b4a38810b5331e41b2c288b852b64b1f4cd15869
Ragunathan96 <itzzmeraghu@gmail.com>
2017-07-25 12:08:39 +0530

    Add files via upload

---
+++ Hun51.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hun51 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String s1=s.nextLine();
+		String s2=s.nextLine();
+		int mul=Integer.parseInt(s1)*Integer.parseInt(s2);
+		System.out.println(String.valueOf(mul));
+		
+	}
+
+}
